5934aa6ee5f2982fb832b0a6ffecde97a6ae7084
LP653 <123456789+alexispope693@gmail.com>
2026-01-13 10:08:02 -0600

    Updated code with new functions and README with new pictures.

--- README.md
+++ README.md
@@ -2,15 +2,18 @@
 
 ## Description
 
-This is my project for a 1306 128x64 OLED screen with an ESP32 over I2C. 
+This is my project for a 1306 128x64 OLED screen with an ESP32 over I2C.
 
-**Demo**
-[![ESP32 Controlled Screen Demo](./images/sin.PNG)](./images/demo.mov)
+[![ESP32 Controlled Screen Demo](./images/Sin_demo.png)](https://youtu.be/hutwwg36yZc)
+
+**Demo Video:** https://youtu.be/hutwwg36yZc
 
 ***
 The program `oled_display.ino` is a fork of KrisKasprzak's OLED_Graphing.ino file, found through his youtube video:
 
 [![Source Video](./images/YouTube_thumbnail.png)](https://www.youtube.com/watch?v=13PFOwcK3-I&ab_channel=KrisKasprzak)
+
+https://www.youtube.com/watch?v=13PFOwcK3-I&ab_channel=KrisKasprzak
 
 ***
 
@@ -20,13 +23,29 @@ It displays an animation drawing of a function. It has 4 functions to choose fro
 
 | **Sin**                      | **Cos**                   |
 | ---------------------------- | ------------------------- |
-| ![Sin](./images/sin.PNG)       | ![Cos](./images/cos.PNG)    |
+| ![Sin](./images/Sin_demo.PNG)       | ![Cos](./images/Cos_signal.png)    |
 
-| **Square**                   | **Noise**                 |
+| **Square**                   | **Triangle**                 |
 | ---------------------------- | ------------------------- |
-| ![Square](./images/square.PNG) | ![Noise](./images/noise.PNG)|
+| ![Square](./images/Square_Signal.PNG) | ![Triangle](./images/Triangle_Signal.PNG)|
+
+| **Sawtooth**                   | **Noise**                 |
+| ---------------------------- | ------------------------- |
+| ![Sawtooth](./images/Sawtooth_Signal.PNG) | ![Noise](./images/Random_Signal.PNG)|
 
 By changing the mode parameter and adjusting the `checkMode()` function, one can add more functions to animate.
+
+The `phase` and `frequency` can be altered with the other knobs and affect all the modes (except for the `Noise` signal).
+
+It can also change display modes, but this must be done through the code.
+
+|**Labeled**|**Center X-Axis**|
+|----|-----|
+|![Labeled Display](images/Labeled_Graph.png)|![Center X-axis](images/X_Center_Axes.png)|
+
+|**Center Y-Axis**|**Center Both Axes**|
+|------|--------|
+|![Center Y-axis](images/Y_Center_Axes.png)|![Center both axes](images/Both_Middle_Axes.png)|
 
 ## Parts List
 
@@ -46,122 +65,9 @@ By changing the mode parameter and adjusting the `checkMode()` function, one can
 
 [![1306 Link](/images/screen.png)](https://app.cirkitdesigner.com/project/4532d6f5-5b08-490d-a55c-c5690e6aef9a)
 
-## Block Diagram
+## Code Description
 
-![Block Diagram](images/block%20diagram/block_diagram.png)
-
-## Pseudocode
-
-```
-
-int mode = 1;
-double x = 0;
-double y = 0;
-
-double phase = 0;
-double freq = 1;
-
-bool redraw = true;
-
-int xmin = 0;
-int xmax = 100;
-int ymin = 0;
-int ymax = 64;
-
-void setup() {
-  Serial.begin(9600);
-
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-  display.display(); // Display Adafruit Logo
-  delay(2000);
-
-  display.clearDisplay();
-  delay(2000);
-}
-
-void loop() {
-
-  mode = read(MODE_PIN);
-
-  freq = read(FREQ_PIN);
-  phase = read(PHASE_PIN);
-  
-  checkMode();
-
-}
-
-void checkMode() {
-  switch (mode) {
-    case 1:
-      graphSin();
-      break;
-    case 2: 
-      graphCos();
-      break;
-    case 3:
-      graphSquare();
-      break;
-    case 4:
-      graphNoise();
-      break;
-    default:
-      break;
-  }
-}
-
-void graphNoise() {
-  float rand = 1.0*random(ymin, ymax);
-  
-  graph(display, x++, rand, "Random Number");
-}
-
-void graphSin() {
-  double y = ymax*(sin(2*M_PI*(freq*x/xmax + phase))+1)/2;
-  graph(display, x++, val, "Sin Graph");
-}
-
-void graphCos() {
-  double val = ymax*(cos(2*M_PI*(freq*x/xmax + phase))+1)/2;
-  graph(display, x++, val, "Cos Graph");
-}
-
-void graphSquare() {
-  double val = cos(2*M_PI*(freq*x/xmax + phase)) > 0 ? ymax - 1 : ymin + 1;
-  graph(display, x++, val, "Square Graph");
-}
-
-void redrawAxes() {
-    drawTitle(); // Filled Rectangle with Text at top of screen
-
-    drawY_Axis();
-    drawX_Axis();
-}
-
-void graph(Adafruit_SSD1306 &display, double x, double y, String title) {
-    if (x == 100) {
-        // Reset
-        x = 0;
-        display.clearDisplay();
-
-        // Initialize
-        drawTitle();
-        drawY_Axis();
-        drawX_Axis();
-    }
-
-    // Draw line segment to add data
-    display.drawLine(ox, oy, x, y, white);
-    ox = x;
-    oy = y;
-}
-```
-
-**Code Description**
-
-*Setup*
+***Setup***
 
 It uses a global `x` variable to and each call of the `graph()` function must increase x: `x++`.
 
@@ -170,8 +76,9 @@ It also uses global `mode`, `freq`, and `phase` variables that are attached to e
 After initializing the ESP32's Serial interface in the `setup()` function, it then checks if the 1306 OLED screen is properly connected.
 
 The program then displays the pre-loaded Adafruit logo before clearing the screen. It then moves to the `loop()` function.
+******
 
-*Loop*
+***Loop***
 
 The `loop()` function first reads each of the potentiometer values and assigns them to corresponding variables.
 
@@ -179,10 +86,6 @@ It then calls the `checkMode()` function, which determines what graph to animate
 
 ## Conclusion & Further Work
 
-I intend to add both triangle and sawtooth wave functions.
+I would like to make the graph fade out before it loops, so it's not as jarring. This also might make switching modes look much better.
 
-In addition to multiple axes options (i.e. x-axis centered, y-axis centered, etc.), this will likely include different functions for each option.
-
-I would also like to make the fade out before it loops, so it's not as jarring.
-
-For future projects, this will be a real-time display for audio signals.
+For future projects, this could be a real-time display for audio signals, as well as UIs for synths.
--- oled_display.ino
+++ oled_display.ino
@@ -4,6 +4,7 @@ https://github.com/KrisKasprzak/96_Graphing/blob/main/OLED_Graphing.ino
 
 */
 
+#include <cmath>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -12,8 +13,8 @@ https://github.com/KrisKasprzak/96_Graphing/blob/main/OLED_Graphing.ino
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 
-#define FREQ_PIN 32
-#define PHASE_PIN 33
+#define FREQ_PIN 25
+#define PHASE_PIN 12
 
 #define MODE_PIN 34
 
@@ -21,33 +22,50 @@ https://github.com/KrisKasprzak/96_Graphing/blob/main/OLED_Graphing.ino
 #define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-int modes = 4;
+const int modes = 6;
+int mode = 1; // global variable used to pass the value of mode pin, range from 1 to *modes*
 
-int mode = 1;
-double x = 0; // global variable
-double y = 0;
-double gx = 30;
-double gy = 50;
-double w = 75;
-double h = 30;
-double dig = 0;
-int max_freq = 10;
-int min_freq = 1;
-double min_phase = 0.0;
-double max_phase = 1.0;
-double phase = 0;
-double freq = 1;
-double randomNum = 0.0;
-double ox, oy;
+const int xmin = 0;
+const int xmax = 100;
+const int xrange = xmax - xmin;
 
-int xmin = 0;
-int xmax = 100;
-int ymin = 0;
-int ymax = 64;
+const int ymin = 0;
+const int ymax = 64;
+const int yrange = ymax - ymin;
+
+int x = 0; // global variable to track horizontal data position (within xrange)
+
+double gx = 10; // horizontal displacement of graph (0-128), positive is right, starts at left side
+double gy = 60; // vertical         "      "   "" of graph (0-64), positive is down, starts at top
+
+double w = 110; // width of graph
+double h = 58; // height of graph
+
+// variables for axis markers
 int xsteps = 4;
 double xinc = (xmax - xmin)/xsteps;
+
 int ysteps = 2;
 double yinc = (ymax - ymin)/ysteps;
+
+double dig =  0; // digit for labeled axes
+
+const double max_freq = 10.0;
+const double min_freq = 1.0;
+
+const double min_phase = 0.0;
+const double max_phase = 1.0;
+
+double phase = 0; // global variable similar to mode but for phase
+double freq = 1.0; // global variable   "    "   "    "   "  frequency
+
+// placeholders to draw lines connecting data points
+// initialized at (0,0)
+double ox = gx;
+double oy = gy-h/2; // just gy for x axis at bottom of screen
+
+int triangle_data[xrange];
+// possibly introduce a sin_data array to use for sin calculations and use sin(x) = cos(x-pi/2)
 
 void setup() {
   Serial.begin(9600);
@@ -61,6 +79,10 @@ void setup() {
 
   display.clearDisplay();
   delay(2000);
+
+  // Produce data for triangle wave
+  for (int i = 0; i < trunc(xrange/2); i++) triangle_data[i] = 2*(i-xmin)*(ymax-ymin)/xrange; // positive slope
+  for (int i = ceil(xrange/2); i < xrange; i++) triangle_data[i] = 2*(xmax-i)*(ymax-ymin)/xrange; // negative slope
 }
 
 void loop() {
@@ -74,31 +96,59 @@ void loop() {
 
 }
 
-/*
+void graph(Adafruit_SSD1306 &d, double y, String title) {
 
-  function to draw a cartesian coordinate system and plot whatever data you want
-  just pass x and y and the graph will be drawn huge arguement list
+  if (x > xmax) x = 0;
+  if (x == 0) {
 
-  &display to pass the display object, mainly used if multiple displays are connected to the MCU
-  x = x data point
-  y = y datapont
-  gx = x graph location (lower left)
-  gy = y graph location (lower left)
-  w = width of graph
-  h = height of graph
-  xlo = lower bound of x axis
-  xhi = upper bound of x asis
-  xinc = division of x axis (distance not count)
-  ylo = lower bound of y axis
-  yhi = upper bound of y asis
-  yinc = division of y axis (distance not count)
-  title = title of graph
-  &redraw = flag to redraw graph on fist call only
+    ox = gx;
 
-*/
-void graph(Adafruit_SSD1306 &d, double y, double gx, double gy, double w, double h, double xlo, double xhi, double xinc, double ylo, double yhi, double yinc, double dig, String title) {
+    double temp;
+    double i;
+    display.clearDisplay();
 
-  if (x > 100) x = 0;
+    // draw y scale
+    for (i = ymin; i <= ymax; i += yinc) {
+      // compute the transform
+      // note: their transform function is the same as the map fucntion, except the map uses long and we need double
+      temp = (i - ymin)*(gy - h - gy)/(ymax - ymin) + gy;
+      if (i == ((ymax-ymin)/2-(ymax-ymin)%2)) d.drawFastHLine(gx-3, temp, w+3, SSD1306_WHITE); // draw x axis
+      else d.drawFastHLine(gx-3+w/2, temp, 3, SSD1306_WHITE);
+      d.setCursor(gx-27, temp-3);
+    }
+
+    // draw x scale
+    for (i = xmin; i <= xmax; i += xinc) {
+      //compute the transform
+      temp = (i-xmin)*w/(xmax-xmin)+gx;
+      if (i == ((xmax-xmin)/2-(xmax-xmin)%2)) d.drawFastVLine(temp, gy-h, h+3, SSD1306_WHITE); // draw y axis
+      else d.drawFastVLine(temp, gy-h/2, 3, SSD1306_WHITE);
+      d.setCursor(temp, gy+6);
+    }
+  }
+
+  // graph drawn now plot the data
+
+  double u = (x-xmin)*w/(xmax-xmin) + gx;
+  y = (y-ymin)*(gy-h-gy)/(ymax-ymin)+gy;
+  d.drawLine(ox, oy, u, y, SSD1306_WHITE);
+  d.drawLine(ox, oy, u, y-1, SSD1306_WHITE);
+  ox = u;
+  oy = y;
+
+  d.display();
+}
+
+/* Alternate graphing functions
+void graphLabeledAxes(Adafruit_SSD1306 &d, double y, String title) {
+  // alter gx/gy and w/h to fit labels
+  double gx = 20;
+  double gy = 50;
+  double w = 90;
+  double h = 27;
+  double ox = gx;
+  double oy = gy;
+  if (x > xmax) x = 0;
   if (x == 0) {
 
     double temp;
@@ -111,28 +161,26 @@ void graph(Adafruit_SSD1306 &d, double y, double gx, double gy, double w, double
     d.setTextSize(1);
     d.setCursor(2, 4);
     d.println(title);
-    ox = (x - xlo) * ( w) / (xhi - xlo) + gx;
-    oy = (y - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
 
     // draw y scale
     d.setTextSize(1);
     d.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
-    for (i = ylo; i <= yhi; i += yinc) {
+    for (i = ymin; i <= ymax; i += yinc) {
       // compute the transform
       // note: their transform function is the same as the map fucntion, except the map uses long and we need double
-      temp = (i - ylo)*(gy - h - gy)/(yhi - ylo) + gy;
+      temp = (i - ymin)*(gy - h - gy)/(ymax - ymin) + gy;
       if (i == 0) d.drawFastHLine(gx-3, temp, w+3, SSD1306_WHITE);
       else d.drawFastHLine(gx-3, temp, 3, SSD1306_WHITE);
-      d.setCursor(gx-27, temp-3);
+      d.setCursor(gx-18, temp-3);
       d.println(i, dig);
     }
 
     // draw x scale
-    for (i = xlo; i <= xhi; i += xinc) {
+    for (i = xmin; i <= xmax; i += xinc) {
       //compute the transform
       d.setTextSize(1);
       d.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
-      temp = (i-xlo)*w/(xhi-xlo)+gx;
+      temp = (i-xmin)*w/(xmax-xmin)+gx;
       if (i == 0) d.drawFastVLine(temp, gy-h, h+3, SSD1306_WHITE);
       else d.drawFastVLine(temp, gy, 3, SSD1306_WHITE);
       d.setCursor(temp, gy+6);
@@ -142,56 +190,247 @@ void graph(Adafruit_SSD1306 &d, double y, double gx, double gy, double w, double
 
   // graph drawn now plot the data
 
-  double u = (x-xlo)*w/(xhi-xlo) + gx;
-  y = (y-ylo)*(gy-h-gy)/(yhi-ylo)+gy;
+  double u = (x-xmin)*w/(xmax-xmin) + gx;
+  y = (y-ymin)*(gy-h-gy)/(ymax-ymin)+gy;
+  d.drawLine(u, y, u, y, SSD1306_WHITE);
+  d.drawLine(u, y-1, u, y-1, SSD1306_WHITE);
+
+  d.display();
+}
+
+void graphLabelessAxes(Adafruit_SSD1306 &d, double y, String title) {
+
+  if (x > xmax) x = 0;
+  if (x == 0) {
+
+    double temp;
+    double i;
+    display.clearDisplay();
+
+    // draw y scale
+    for (i = ymin; i <= ymax; i += yinc) {
+      // compute the transform
+      // note: their transform function is the same as the map fucntion, except the map uses long and we need double
+      temp = (i - ymin)*(gy - h - gy)/(ymax - ymin) + gy;
+      if (i == 0) d.drawFastHLine(gx-3, temp, w+3, SSD1306_WHITE);
+      else d.drawFastHLine(gx-3, temp, 3, SSD1306_WHITE);
+      d.setCursor(gx-27, temp-3);
+    }
+
+    // draw x scale
+    for (i = xmin; i <= xmax; i += xinc) {
+      //compute the transform
+      temp = (i-xmin)*w/(xmax-xmin)+gx;
+      if (i == 0) d.drawFastVLine(temp, gy-h, h+3, SSD1306_WHITE);
+      else d.drawFastVLine(temp, gy, 3, SSD1306_WHITE);
+      d.setCursor(temp, gy+6);
+    }
+  }
+
+  // graph drawn now plot the data
+
+  double u = (x-xmin)*w/(xmax-xmin) + gx;
+  y = (y-ymin)*(gy-h-gy)/(ymax-ymin)+gy;
+  d.drawLine(u, y, u, y-2, SSD1306_WHITE);
+  d.drawLine(u, y-2, u, y, SSD1306_WHITE);
+
+  d.display();
+}
+
+void graphCenterY_Axis(Adafruit_SSD1306 &d, double y, String title) {
+  if (x > xmax) x = 0;
+  if (x == 0) {
+
+    ox = gx;
+
+    double temp;
+    double i;
+    display.clearDisplay();
+
+    // draw y scale
+    for (i = ymin; i <= ymax; i += yinc) {
+      // compute the transform
+      // note: their transform function is the same as the map fucntion, except the map uses long and we need double
+      temp = (i - ymin)*(gy - h - gy)/(ymax - ymin) + gy;
+      if (i == 0) d.drawFastHLine(gx-3, temp, w+3, SSD1306_WHITE); // draw x axis
+      else d.drawFastHLine(gx-3+w/2, temp, 3, SSD1306_WHITE);
+      d.setCursor(gx-27, temp-3);
+    }
+
+    // draw x scale
+    for (i = xmin; i <= xmax; i += xinc) {
+      //compute the transform
+      temp = (i-xmin)*w/(xmax-xmin)+gx;
+      if (i == ((xmax-xmin)/2-(xmax-xmin)%2)) d.drawFastVLine(temp, gy-h, h+3, SSD1306_WHITE); // draw y axis
+      else d.drawFastVLine(temp, gy, 3, SSD1306_WHITE);
+      d.setCursor(temp, gy+6);
+    }
+  }
+
+  // graph drawn now plot the data
+
+  double u = (x-xmin)*w/(xmax-xmin) + gx;
+  y = (y-ymin)*(gy-h-gy)/(ymax-ymin)+gy;
   d.drawLine(ox, oy, u, y, SSD1306_WHITE);
-  d.drawLine(ox, oy-1, u, y-1, SSD1306_WHITE);
+  d.drawLine(ox, oy, u, y-1, SSD1306_WHITE);
   ox = u;
   oy = y;
 
   d.display();
 }
 
+void graphCenterX_Axis(Adafruit_SSD1306 &d, double y, String title) {
+  if (x > xmax) x = 0;
+  if (x == 0) {
+    ox = gx;
+    oy = gy - h/2;
+
+    double temp;
+    double i;
+    display.clearDisplay();
+
+    // draw y scale
+    for (i = ymin; i <= ymax; i += yinc) {
+      // compute the transform
+      // note: their transform function is the same as the map fucntion, except the map uses long and we need double
+      temp = (i - ymin)*(gy - h - gy)/(ymax - ymin) + gy;
+      if (i == ((ymax-ymin)/2-(ymax-ymin)%2)) d.drawFastHLine(gx-3, temp, w+3, SSD1306_WHITE);
+      else d.drawFastHLine(gx-3, temp, 3, SSD1306_WHITE);
+      d.setCursor(gx-27, temp-3);
+    }
+
+    // draw x scale
+    for (i = xmin; i <= xmax; i += xinc) {
+      //compute the transform
+      temp = (i-xmin)*w/(xmax-xmin)+gx;
+      if (i == 0) d.drawFastVLine(temp, gy-h, h+3, SSD1306_WHITE);
+      else d.drawFastVLine(temp, gy-h/2, 3, SSD1306_WHITE);
+      d.setCursor(temp, gy+6);
+    }
+  }
+
+  // graph drawn now plot the data
+
+  double u = (x-xmin)*w/(xmax-xmin) + gx;
+  y = (y-ymin)*(gy-h-gy)/(ymax-ymin)+gy;
+  d.drawLine(u, y, u, y-2, SSD1306_WHITE);
+  d.drawLine(u, y-2, u, y, SSD1306_WHITE);
+
+  d.display();
+}
+
+void graphCenterBothAxis(Adafruit_SSD1306 &d, double y, String title) {
+  if (x > xmax) x = 0;
+  if (x == 0) {
+
+    ox = gx;
+
+    double temp;
+    double i;
+    display.clearDisplay();
+
+    // draw y scale
+    for (i = ymin; i <= ymax; i += yinc) {
+      // compute the transform
+      // note: their transform function is the same as the map fucntion, except the map uses long and we need double
+      temp = (i - ymin)*(gy - h - gy)/(ymax - ymin) + gy;
+      if (i == ((ymax-ymin)/2-(ymax-ymin)%2)) d.drawFastHLine(gx-3, temp, w+3, SSD1306_WHITE); // draw x axis
+      else d.drawFastHLine(gx-3+w/2, temp, 3, SSD1306_WHITE);
+      d.setCursor(gx-27, temp-3);
+    }
+
+    // draw x scale
+    for (i = xmin; i <= xmax; i += xinc) {
+      //compute the transform
+      temp = (i-xmin)*w/(xmax-xmin)+gx;
+      if (i == ((xmax-xmin)/2-(xmax-xmin)%2)) d.drawFastVLine(temp, gy-h, h+3, SSD1306_WHITE); // draw y axis
+      else d.drawFastVLine(temp, gy-h/2, 3, SSD1306_WHITE);
+      d.setCursor(temp, gy+6);
+    }
+  }
+
+  // graph drawn now plot the data
+
+  double u = (x-xmin)*w/(xmax-xmin) + gx;
+  y = (y-ymin)*(gy-h-gy)/(ymax-ymin)+gy;
+  d.drawLine(ox, oy, u, y, SSD1306_WHITE);
+  d.drawLine(ox, oy, u, y-1, SSD1306_WHITE);
+  ox = u;
+  oy = y;
+
+  d.display();
+}
+*/
+
 void checkMode() {
   switch (mode) {
-    case 1:
-      graphSin();
+    case 1: // Sin
+      graph(display, yrange*(sin(2*M_PI*(1.0*freq*x/xrange - phase))+1)/2, "Sin Graph");
+      x++;
       break;
-    case 2: 
-      graphCos();
+    case 2: // Cos
+      graph(display, yrange*(cos(2*M_PI*(1.0*freq*x/xrange - phase))+1)/2, "Cos Graph");
+      x++;
       break;
-    case 3:
-      graphSquare();
+    case 3: // Square
+      graph(display, sin(2*M_PI*(1.0*freq*x/xrange - phase)) > 0 ? ymax - 1 : ymin + 1, "Square Graph");
+      x++;
       break;
-    case 4:
-      graphNoise();
+    case 4: // Triangle
+      graph(display, triangle_data[((int) (freq*x - phase*xrange))%xrange], "Triangle Graph");
+      x++;
+      break;
+    case 5: // Sawtooth
+      graphSawtooth(); // must be a function because it defines an internal variable
+      break;
+    case 6: // Noise
+      graph(display, 1.0*random(ymin, ymax), "Random Number");
+      x++;
       break;
     default:
       break;
   }
 }
 
-void graphNoise() {
-  int rand = random(ymin, ymax);
-  randomNum = 1.0*rand;
-  graph(display, randomNum, gx, gy, w, h, xmin, xmax, xinc, ymin, ymax, yinc, dig, "Random Number");
+void graphSawtooth() {
+  // point slope equation with the modulus
+  int val = ((int) (freq*(x-xmin-xrange*phase/freq)*yrange/(xrange)))%ymax;
+  if (val < 0) val += yrange; // Check if the phase variable "pushes" it negative and compensate by adding yrange because it can only happen within the phase, so it will always bring it within view
+  graph(display, val, "Sawtooth Graph");
   x++;
 }
 
-void graphSin() {
-  double val = ymax*(sin(2*M_PI*(freq*x/xmax + phase))+1)/2;
-  graph(display, val, gx, gy, w, h, xmin, xmax, xinc, ymin, ymax, yinc, dig, "Sin Graph");
-  x++;
-}
+/* Functional definitions of modes
+  void graphSin() {
+    graph(display, yrange*(sin(2*M_PI*(1.0*freq*x/xrange - phase))+1)/2, "Sin Graph");
+    x++;
+  }
 
-void graphCos() {
-  double val = ymax*(cos(2*M_PI*(freq*x/xmax + phase))+1)/2;
-  graph(display, val, gx, gy, w, h, xmin, xmax, xinc, ymin, ymax, yinc, dig, "Cos Graph");
-  x++;
-}
+  void graphCos() {
+    graph(display, yrange*(cos(2*M_PI*(1.0*freq*x/xrange - phase))+1)/2, "Cos Graph");
+    x++;
+  }
 
-void graphSquare() {
-  double val = sin(2*M_PI*(freq*x/xmax + phase)) > 0 ? ymax - 1 : ymin + 1;
-  graph(display, val, gx, gy, w, h, xmin, xmax, xinc, ymin, ymax, yinc, dig, "Square Graph");
-  x++;
-}
+  void graphSquare() {
+    graph(display, sin(2*M_PI*(1.0*freq*x/xrange - phase)) > 0 ? ymax - 1 : ymin + 1, "Square Graph");
+    x++;
+  }
+
+  void graphTriangle() {
+    graph(display, triangle_data[((int) (freq*x - phase*xrange))%xrange], "Triangle Graph");
+    x++;
+  }
+
+  void graphSawtooth() {
+    // point slope equation with the modulus
+    int val = ((int) (freq*(x-xmin-xrange*phase/freq)*yrange/(xrange)))%ymax;
+    if (val < 0) val += yrange;
+    graph(display, val, "Sawtooth Graph");
+    x++;
+  }
+
+  void graphNoise() {
+    graph(display, 1.0*random(ymin, ymax), "Random Number");
+    x++;
+  }
+*/
